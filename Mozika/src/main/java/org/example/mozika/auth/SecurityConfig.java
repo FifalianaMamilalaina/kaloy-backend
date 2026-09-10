2a84fb4e11338cf353eb4795c7588b2566291b52
Antonia Bijou <163192297+AntoniaBijou@users.noreply.github.com>
2026-09-10 12:44:57 +0300

    Update SecurityConfig.java

--- Mozika/src/main/java/org/example/mozika/auth/SecurityConfig.java
+++ Mozika/src/main/java/org/example/mozika/auth/SecurityConfig.java
@@ -46,7 +46,13 @@ public class SecurityConfig {
                     "/swagger-ui/**",
                     "/swagger-ui.html",
                     "/v3/api-docs/**",
-                    "/v3/api-docs"
+                    "/v3/api-docs",
+                    "/artists/**",
+                    "/albums/**",
+                    "/songs/**",
+                    "/genres/**",
+                    "/editorialplaylists/**",
+                    "/events/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
